f5d8ad04c4ce2f26948be3d764efde6acde02aea
이주영 <wruomma2@naver.com>
2025-07-13 17:08:14 +0900

    feat : 프로젝트 생성 및 세팅

---
+++ src/main/java/com/project/homepage_v2/HomepageV2Application.java
@@ -0,0 +1,13 @@
+package com.project.homepage_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepageV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomepageV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/homepage_v2/HomepageV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.homepage_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomepageV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
